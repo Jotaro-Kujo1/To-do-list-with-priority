b371327490a3e73a0a11677cc8ea21afab5b9ad6
Vadim <lebedev.vada@gmail.com>
2022-01-15 20:52:43 +0300

    Добавлен базовый дизайн

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,28 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.CheckBox;
+import javafx.geometry.Orientation;
+import javafx.geometry.Insets;
+
+public class Main extends Application {
+
+    ListShaping ls;
+
     public static void main(String[] args) {
-        //Treatment tr = new Treatment();
-        //tr.arraysSplit();
-        //tr.display();
-        //tr.display2();
-        //tr.display3();
-        ListShaping ls = new ListShaping();
+        Application.launch(args);
+    }
+
+    public void start(Stage stage) throws Exception{
+
+        ls = new ListShaping();
+        Parent root = FXMLLoader.load(getClass().getResource("shablon.fxml"));
+        stage.setTitle(" Список дел ");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
