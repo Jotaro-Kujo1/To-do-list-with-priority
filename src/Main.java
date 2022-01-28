78b534537b5f8d9fe2cab5d098dbf04f1891a7a4
Vadim <lebedev.vada@gmail.com>
2022-01-28 20:12:50 +0300

    Добавлен вывод дизайна

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,20 @@ import javafx.geometry.Insets;
 
 import java.util.List;
 
-public class Main {
+public class Main extends Application{
 
     public static void main(String[] args) {
-        ListShaping ls = new ListShaping();
-        List<Event> ev = ls.eventCreating();
-        ev.forEach(System.out::println);
+        Application.launch(args);
+        //ListShaping ls = new ListShaping();
+        //List<Event> ev = ls.eventCreating();
+        //ev.forEach(System.out::println);
+    }
+
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("shablon.fxml"));
+        stage.setTitle("To - do list");
+        stage.setScene(new Scene(root,400,400));
+        stage.show();
     }
 
 }
