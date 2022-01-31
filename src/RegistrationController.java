62ac20089df0df7313f64efa15f2b0882eba005b
Vadim <lebedev.vada@gmail.com>
2022-01-31 14:49:36 +0300

    Добавлено окно регистрации

---
+++ src/RegistrationController.java
@@ -0,0 +1,30 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class RegistrationController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField loginReg;
+
+    @FXML
+    private TextField passwordReg;
+
+    @FXML
+    private Button signUpButtonReg;
+
+    @FXML
+    void initialize() {
+
+
+    }
+}
+
